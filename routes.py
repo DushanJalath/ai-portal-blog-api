60c4a15b04063955229b07f834cab7d785364fa4
Nadil Siriwardhana <nadildinsarasiriwardhana@gmail.com>
2024-06-10 10:39:35 +0530

    added getBlogsBytags to routes.py and services.py

--- routes.py
+++ routes.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter,Depends,HTTPException
-from services import initial_service
+from services import initial_service,get_blogs_byTags
+from typing import List
+from models import BlogPost
 
 
 
@@ -8,3 +10,7 @@ router=APIRouter()
 @router.get('/')
 async def getInitial():
     return initial_service()
+
+@router.get('/blogsByTags')
+async def Blogs_By_tags(tags : List[int]):
+    return get_blogs_byTags(tags)
--- services.py
+++ services.py
@@ -1,4 +1,17 @@
 from fastapi import HTTPException
+from database import collection_blog
+from models import BlogPost
 
 def initial_service():
     return {"Ping":"Pong"}
+
+def get_blogs_byTags(tags):
+    blogs=[]
+    cursor=collection_blog.find({"tags":tags})
+    for document in cursor:
+        blogs.append(BlogPost(**document))
+    
+    if not blogs:  # if the blogs list is empty
+        return [{"message": "No blogs found for these tags"}]
+    else:
+        return blogs
