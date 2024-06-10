c5d99944da6070e07598695640acd2f60b081dce
Nadil Siriwardhana <nadildinsarasiriwardhana@gmail.com>
2024-06-10 22:30:48 +0530

    added data type to the parameter of get_blogs_by_tags function and enabled checking at least one tag is available

--- services.py
+++ services.py
@@ -1,15 +1,16 @@
 from fastapi import HTTPException
 from database import collection_blog
 from models import BlogPost
+from typing import List
 
 def initial_service():
     return {"Ping":"Pong"}
 
-def get_blogs_byTags(tags):
+def get_blogs_byTags(tags : List[int]):
     blogs=[]
-    if collection_blog.count_documents({"tags": tags}) == 0:
+    if collection_blog.count_documents({"tags": {"$in": tags}}) == 0:
         raise HTTPException(status_code=404, detail="no blogs found with the given tags.")
-    cursor=collection_blog.find({"tags":tags})
+    cursor=collection_blog.find({"tags": {"$in": tags}})
     for document in cursor:
         blogs.append(BlogPost(**document))
     return blogs
