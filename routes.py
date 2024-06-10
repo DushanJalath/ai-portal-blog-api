7b2f800cabf29d043b500fef66c000ebb04a0ae3
Roshana Isuranga <roshanaisuran2002@gmail.com>
2024-06-10 22:09:03 +0530

    Modified routes.py - createblog and updateBlog

--- routes.py
+++ routes.py
@@ -12,12 +12,8 @@ async def getInitial():
 
 @router.post('/createblog', response_model=Blog)
 async def createBlog(blog: Blog):
-    response = await create_blog(blog)
-    if response:
-        return response
+    return await create_blog(blog)
 
 @router.put('/updateblog{id}', response_model=Blog)
 async def updateBlog(id: str, title:str, content:str):
-    response = await update_blog(id, title, content)
-    if response:
-        return response
+    return await update_blog(id, title, content)
